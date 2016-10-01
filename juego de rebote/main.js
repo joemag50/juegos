53db4526d3f5ca02592f2fd012b2519cef878066
jose gamboa <greengamboa@gmail.com>
2016-10-01 00:41:19 -0500

    Ahora choca con las paredes y se detiene, se agrego la carpeta de assets

--- juego de rebote/main.js
+++ juego de rebote/main.js
@@ -1,74 +1,73 @@
 // Initialize Phaser and creates a game
 var game = new Phaser.Game(400, 600, Phaser.AUTO, 'gameDiv');
 
-var player, enemigo, muro2;
-var direccion = true;
-var nueva_posicion;
+//variables de personajes
+var player, enemigo, fondo;
+//variables de control
+var direccion = true, backgroundv, nueva_posicion;
 
 // Creates a new 'main' state that will contain the game
-var mainState = {
-
+var mainState =
+{
     // Function for loading all assets of the game (called only once)
-    preload: function() { 
+    preload: function()
+    { 
         //precargamos la imagen del mono
+        game.load.image('fondo','assets/fondo.png');
         game.load.image('player','assets/player.png');
         game.load.image('enemigo','assets/enemigo.png');
-        game.load.image('muro','assets/muro.png');
     },
-
     // Fuction called o after 'preload' to setup the game (called only once)
-    create: function() {
+    create: function()
+    {
         //dibujamos al mono
-        player  = game.add.sprite(200,500,'player');
+        fondo   = game.add.tileSprite(0,0,400,600,'fondo');
+        player  = game.add.sprite(game.world.centerX,game.world.centerY + 200,'player');
         enemigo = game.add.sprite(10,0,'enemigo');
-        muro    = game.add.sprite(380,500,'muro');
-        muro2   = game.add.sprite(-50,500,'muro');
-
+        
         // La gravedad, la utilizaremos para los objetos que caen
         game.physics.startSystem(Phaser.Physics.ARCADE);
-        
-        game.physics.arcade.enable(enemigo);
+        game.physics.arcade.enable([enemigo,player]);
+
         enemigo.body.gravity.y = 50;
         enemigo.body.velocity.y = 100;
+        //Esto hace que se quede pegado en el muro OMG!!
+        player.body.collideWorldBounds = true;
 
-        //funcion del movimiento
-        game.input.onDown.add(this.muevete,this);
+        //Funciones sobre movimiento
+        //onTap evalua un tap o dobletap
+        game.input.onTap.add(this.onTap,this);
+        backgroundv = 2;  //Velocidad del fondo
     },
-
     // This function is called 60 times per second
-    update: function() {
-    	
-    	if (player.x > 360){
-    		direccion = false;
-    	}
-    	else if (player.x < 15){
-    		direccion = true;
-    	}
-
-    	//movimiento
-        if (direccion == true) {
-            player.x = player.x + 4;
+    update: function()
+    {
+        fondo.tilePosition.y += backgroundv;
+        
+        if (direccion == true)
+        {
+            player.body.velocity.x = 250;
         }
-        else if (direccion == false) {
-            player.x = player.x - 4;
-    	}
-    	else{
-    		player.x = player.x;
-    	}
-
+        else
+        {
+            player.body.velocity.x = -250;
+        }
+        
+        //movimiento
+        
         nueva_posicion = Math.floor(Math.random() * 399) + 1;
-        if (enemigo.y > 600){
-        	enemigo.x = nueva_posicion;
-        	enemigo.y = 0;
+        if (enemigo.y > 600)
+        {
+            enemigo.x = nueva_posicion;
+            enemigo.y = 0;
             enemigo.body.velocity.y = 100;
         }
     },
-    muevete: function() {
-        if (direccion == true){
-            direccion = false;
-        }
-        else{
-            direccion = true;
+    onTap: function(pointer, doubleTap)
+    {
+        if (!doubleTap)
+        {
+            direccion = (direccion === false) ? true : false;
         }
     }
 };
