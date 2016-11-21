892a457c2ad04938ca4150cba4b64e59e6d13441
Jose Gamboa <greengamboa@gmail.com>
2016-11-21 11:02:36 -0600

    Juego de unity

---
+++ juego de unity/Assets/scripts/PlayerSpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpawner : MonoBehaviour
+{
+
+	public GameObject playerPrefab;
+	GameObject playerInstance;
+
+	//float respawnTimer;
+	// Use this for initialization
+	void Start ()
+	{
+		SpawnPlayer();
+	}
+	
+	void SpawnPlayer()
+	{
+		//respawnTimer = 1;
+		playerInstance = (GameObject)Instantiate(playerPrefab, transform.position, Quaternion.identity);		
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		//Si quieres el jugador reaparesca
+		//if(playerInstance == null)
+		//{
+		//	respawnTimer -= Time.deltaTime;
+		//	if(respawnTimer <= 0)
+		//	{
+		//		SpawnPlayer();				
+		//	}
+		//}
+	}
+}
